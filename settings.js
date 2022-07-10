79889346e2f95400305070736e59c58bb9084ba0
AmakawaZKN <108383788+AmakawaZKN@users.noreply.github.com>
2022-07-10 22:35:29 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -37,23 +37,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['6285822347348'] //ur owner number
-global.ownername = "ArullOfc" //ur owner name
-global.ytname = "YT: ARULL OFC" //ur yt chanel name
-global.socialm = "GitHub: Warikrr" //ur github or insta name
-global.location = "Indonesia, Kalimantan Selatan, Kotabaru" //ur location
+global.vcardowner = ['628979440862'] //ur owner number
+global.ownername = "𝙕𝙮𝙠𝙪𝙖𝙣" //ur owner name
+global.ytname = "there isn't any" //ur yt chanel name
+global.socialm = "Tiktok: akwzykuan" //ur github or insta name
+global.location = "Japan, Tokyo🎌" //ur location
 
 //bot bomdy 
-global.owner = ['6285822347348']
-global.ownertag = '6285822347348' //ur tag number
-global.botname = 'ArullBotzMD' //ur bot name
+global.owner = ['628979440862']
+global.ownertag = '628979440862' //ur tag number
+global.botname = '𝕵𝖎𝖎𝖐𝖚𝖆𝖓' //ur bot name
 global.linkz = "https://youtube.com/channel/UC1ikkqsPvtWuE_QonsET1yg" //your theme url which will be displayed on whatsapp
-global.websitex = "https://chat.whatsapp.com/GdXv5LxKfDPHJ4WOH2rNu5" //ur website to be displayed
-global.botscript = 'https://youtu.be/k3QkeVdAMOs' //script link
+global.websitex = "https://chat.whatsapp.com/HDockZJ48S82HUhsxu85kH" //ur website to be displayed
+global.botscript = '' //script link
 global.reactmoji = "🗿" //ur menu react emoji
 global.themeemoji = "😩" //ur theme emoji
-global.packname = "Sticker by" //ur sticker watermark packname
-global.author = "ArullOfc" //ur sticker watermark author
+global.packname = "𝕵𝖎𝖎𝖐𝖚𝖆𝖓" //ur sticker watermark packname
+global.author = "𝙕𝙮𝙠𝙪𝙖𝙣" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -62,7 +62,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['6285822347348'] //ur premium numbers
+global.premium = ['628979440862'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
@@ -88,7 +88,7 @@ global.antitags = true
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
-    success: 'Selesai ✓',
+    success: 'Selesai|𝙕𝙮𝙠𝙪𝙖𝙣',
     admin: 'Fitur Ini Hanya Untuk Admin!',
     botAdmin: 'Bot Harus Admin Dulu!',
     owner: 'Fitur Ini Hanya Untuk Pemilik!',
@@ -99,8 +99,8 @@ global.mess = {
     linkm: 'Mana linknya?',
     error: 'Kesalahan!!',
     endLimit: 'Batas Harian Anda Telah Kedaluwarsa, Batas Akan Direset Setiap 12 Jam',
-    ban: 'Anda telah diblokir oleh pemiliknya, jika Anda ingin diblokir, chat pemilik.',
-    nsfw: 'Fitur nsfw belum diaktifkan, silahkan hubungi admin untuk aktivasi',
+    ban: 'Anda telah diblokir oleh pemiliknya, jika Anda ingin unblokir, chat pemilik.',
+    nsfw: 'Samnge? kok butuh nsfw, silahkan hubungi admin untuk aktivasi',
     banChat: 'Bot diblokir di grup ini, harap hubungi pemiliknya untuk membatalkan pemblokiran'
 }
     global.limitawal = {
